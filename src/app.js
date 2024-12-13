dc10a85942611c259e481f5ec0a0cd9844d8b94a
lucas serpillo <88857502+lucasSerpillo@users.noreply.github.com>
2024-12-13 10:44:02 +0000

    Arreglado el problema de recargar la web y generar carta

--- src/app.js
+++ src/app.js
@@ -5,37 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let palo = ["♥", "♣", "♠", "♦"];
-  let numerosCartas = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+let palo = ["♥", "♣", "♠", "♦"];
+let numerosCartas = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-  let BotonGenerador = document.getElementById("generarCarta");
-  let carta = document.getElementById("carta");
+let BotonGenerador = document.getElementById("generarCarta");
+let carta = document.getElementById("carta");
 
-  function generarCartaAleatoria() {
-    let paloAleatorio = Math.floor(Math.random() * palo.length);
-    let numeroAleatorio = Math.floor(Math.random() * numerosCartas.length);
+function generarCartaAleatoria() {
+  let paloAleatorio = Math.floor(Math.random() * palo.length);
+  let numeroAleatorio = Math.floor(Math.random() * numerosCartas.length);
 
-    carta.innerHTML = `
+  carta.innerHTML = `
       <div class="numeros">${numerosCartas[numeroAleatorio]}</div>
       <div class="palo-der ${palo[paloAleatorio]}">${palo[paloAleatorio]}</div>
       <div class="palo-izq ${palo[paloAleatorio]}">${palo[paloAleatorio]}</div>
     `;
-  }
+}
 
-  BotonGenerador.addEventListener("click", generarCartaAleatoria);
+BotonGenerador.addEventListener("click", generarCartaAleatoria);
+
+window.onload = function() {
+  generarCartaAleatoria();
 };
