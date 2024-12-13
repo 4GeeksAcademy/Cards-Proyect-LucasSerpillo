725cd0a7f6abc2a1f296cee169888c3825fa3a11
lucas serpillo <88857502+lucasSerpillo@users.noreply.github.com>
2024-12-13 09:37:31 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo = ["♥", "♣", "♠", "♦"];
+  let numerosCartas = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let BotonGenerador = document.getElementById("generarCarta");
+  let carta = document.getElementById("carta");
+
+  function generarCartaAleatoria() {
+    let paloAleatorio = Math.floor(Math.random() * palo.length);
+    let numeroAleatorio = Math.floor(Math.random() * numerosCartas.length);
+
+    carta.innerHTML = `
+      <div class="numeros">${numerosCartas[numeroAleatorio]}</div>
+      <div class="palo-der ${palo[paloAleatorio]}">${palo[paloAleatorio]}</div>
+      <div class="palo-izq ${palo[paloAleatorio]}">${palo[paloAleatorio]}</div>
+    `;
+  }
+
+  BotonGenerador.addEventListener("click", generarCartaAleatoria);
 };
